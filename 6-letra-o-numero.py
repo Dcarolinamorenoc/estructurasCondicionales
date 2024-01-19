e9df48281c532f381141a99ebd706cfa96778fbb
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-01-19 12:44:53 -0500

    Ejercicio Letra o número

---
+++ 6-letra-o-numero.py
@@ -0,0 +1,13 @@
+#Escriba un programa que determine si un caracter ingresado es letra, número, o ninguno de los dos. En caso que sea letra, determine si es mayúscula o minúscula.
+
+caracter = input("Ingrese un carácter: ")
+
+if caracter.isalpha():
+    if caracter.isupper():
+        print(f"{caracter} es una letra mayúscula.")
+    else:
+        print(f"{caracter} es una letra minúscula.")
+elif caracter.isdigit():
+    print(f"{caracter} es un número.")
+else:
+    print(f"{caracter} no es ni letra ni número.")
