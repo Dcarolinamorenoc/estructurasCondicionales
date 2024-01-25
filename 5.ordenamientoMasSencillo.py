3dcf5847ff69a2a91c30b7701ee74a621cac9960
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-01-25 18:49:46 -0500

    Ejercicio ordenamiento mas sencillo

---
+++ 5.ordenamientoMasSencillo.py
@@ -0,0 +1,8 @@
+#Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
+
+num1= float=input("Querido usuario ingrese el primer número: ")
+num2= float=input("Querido usuario ingrese el segundo número: ")
+if num1<num2:
+    print("Los números quedan ordenados de la siguiente manera:", num1, num2)
+else:
+   print("Los números quedan ordenados de la siguiente manera:", num2, num1)
